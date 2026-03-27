b259387a46c840149dcfc0f05581142ca5e26aa8
gmlogan <graham@loganfamily.co.uk>
2026-03-27 11:27:58 +0000

    Simple tabs

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,24 +1,27 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
-
-import { useColorScheme } from '@/hooks/use-color-scheme';
-
-export const unstable_settings = {
-  anchor: '(tabs)',
-};
+import { Tabs } from "expo-router";
+import { SymbolView } from "expo-symbols";
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    <Tabs>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Pokédex",
+          tabBarIcon: ({ color }) => (
+            <SymbolView name="list.bullet" tintColor={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="favourites"
+        options={{
+          title: "Favourites",
+          tabBarIcon: ({ color }) => (
+            <SymbolView name="star.fill" tintColor={color} />
+          ),
+        }}
+      />
+    </Tabs>
   );
 }
---
+++ app/favourites.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Favourites() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Favourites</Text>
+    </View>
+  );
+}
